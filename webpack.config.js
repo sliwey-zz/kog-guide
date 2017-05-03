fd5526c94bbe82cac721eb4f844cb674a28f1a10
sliwey <qlw1009@gmail.com>
2017-05-03 21:28:07 +0800

    remove openpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextWebpackPlugin = require('extract-text-webpack-plugin');
-const OpenPack = require('openpack');
 const ROOT_PATH = path.resolve(__dirname);
 const DEV_PATH = path.resolve(ROOT_PATH, 'src');
 const BUILD_PATH = path.resolve(ROOT_PATH, 'build');
@@ -94,11 +93,7 @@ module.exports = {
       inject: 'body'
     }),
 
-    new ExtractTextWebpackPlugin('style.css'),
-
-    // new OpenPack({
-    //   lan: true
-    // })
+    new ExtractTextWebpackPlugin('style.css')
   ]
 
 };
